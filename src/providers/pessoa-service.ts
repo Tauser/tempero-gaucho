6fcd3effd20dfdc2d3cc82ec2728999a1d191ffa
Tauser <tauser@gmail.com>
2017-02-12 17:46:23 -0300

    home
    Retirando dados da Home

--- src/providers/pessoa-service.ts
+++ src/providers/pessoa-service.ts
@@ -2,19 +2,11 @@ import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 
-/*
-  Generated class for the PessoaService provider.
-
-  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
-  for more info on providers and Angular 2 DI.
-*/
 @Injectable()
 export class PessoaService {
 
-  //public pessoas: Array<string>;
   private pessoas: Array<any>;
-  //private url: string = "http://www.temperogauchoce.com.br/temperogaucho/api/pessoas";
-  //private url: string="http://localhost/temperogaucho/api/pessoas";
+
 
   constructor(public http: Http) {}
 
@@ -26,15 +18,9 @@ export class PessoaService {
         .subscribe(data => {
           this.pessoas = data.rows;
           console.log(this.pessoas);
-          //this.pessoas = data.data.children;
-          /*this.pessoas.forEach((e, i, a) => {
-            if (!e.data.thumbnail || e.data.thumbnail.indexOf('b.thumbs.redditmedia.com') === -1 ) {
-              e.data.thumbnail = 'http://www.redditstatic.com/icon.png';
-            }
-          })*/
+
           resolve(this.pessoas);
         }, err => console.log(err));
     });
   }
-
 }
